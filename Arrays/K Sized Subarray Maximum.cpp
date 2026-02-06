ea0b0bf6d3974a1629d651bb067ec6aeb4609d1f
Junaid Bin Jahangir <164465375+junaidr04@users.noreply.github.com>
2026-02-06 12:35:13 +0600

    Enhance code readability with comments
    Added comments to clarify the logic of the maxOfSubarrays function.

--- Arrays/K Sized Subarray Maximum.cpp
+++ Arrays/K Sized Subarray Maximum.cpp
@@ -13,15 +13,19 @@ public:
         deque<int> dq;
         for (int i = 0; i < arr.size(); i++)
         {
+            //Remove indices outside window
             while (!dq.empty() && dq.front() <= i - k)
             {
                 dq.pop_front();
             }
+            //remove smaller elemnts
             while (!dq.empty() && arr[i] >= arr[dq.back()])
             {
                 dq.pop_back();
             }
+            //add current index
             dq.push_back(i);
+            //add max to result when window is full
             if (i >= k - 1)
             {
                 ans.push_back(arr[dq.front()]);
@@ -29,4 +33,5 @@ public:
         }
         return ans;
     }
+
 };
